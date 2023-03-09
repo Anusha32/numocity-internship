54b9189fc6a1849f9b29e0a8684f388cff9d8d5a
Anusha32 <anushabr3@gmai.com>
2023-03-09 15:52:17 +0530

    language chagnge added

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,4 +1,5 @@
 const {celciusTemp} = require('./celciusTemp');
+const lang = require('./changeLanguage');
 
 
 function batteryIsOk(temperature, soc, chargeRate, tempUnit) {
@@ -8,6 +9,13 @@ function batteryIsOk(temperature, soc, chargeRate, tempUnit) {
     {name: 'stateOfCharge', input: soc, min: 20, max: 80},
     {name: 'chargeRate', input: chargeRate, min: 0, max: 0.8},
   ];
+
+  temperature = lang.temperature;
+  soc = lang.soc;
+  chargeRate = lang.chargeRate;
+
+  console.log(`${temperature}, ${soc}, ${chargeRate}`);
+
   const isbatteryOk = values.every((item)=>{
     if (item.input < item.min || item.input > item.max) {
       return false;
