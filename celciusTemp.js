87ffb3af9eb3e17eee6adcc850c348f390159fc7
Anusha32 <anushabr3@gmai.com>
2023-03-15 10:46:44 +0530

    update

--- celciusTemp.js
+++
@@ -1,7 +0,0 @@
-function celciusTemp(temperature, tempUnit) {
-  if (tempUnit == 'Fahrenheit') {
-    temperature = ((temperature -32)*5/9 );
-  }
-  return temperature;
-}
-module.exports = {celciusTemp};
